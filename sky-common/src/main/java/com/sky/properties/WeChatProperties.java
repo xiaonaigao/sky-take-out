934f59227bcb1c92e41e3453b048ff055545a2ed
wangzhilei <wangzhilei2022@163.com>
2023-09-19 09:52:52 +0800

    初始代码91+

--- sky-common/src/main/java/com/sky/properties/WeChatProperties.java
+++ sky-common/src/main/java/com/sky/properties/WeChatProperties.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
 @Component
 @ConfigurationProperties(prefix = "sky.wechat")
 @Data
